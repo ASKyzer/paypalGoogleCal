2346722c8d07305a9eb1e937d844961c5bbd941d
Adrian Kyzer <adriankyzer@gmail.com>
2019-02-08 16:24:28 +0100

    cleaned up the js codes

--- app.js
+++ app.js
@@ -52,168 +52,169 @@ paypal.Button.render({
     })
   },
   
-  onAuthorize: function (data, actions) {   
+  onAuthorize: function (data, actions) {
+    document.getElementById('orderNumber').innerText = data.orderId
     return actions.payment.execute()
       .then(function () {
-       // const clientId = 'CLIENT_ID';
-        // const apiKey = 'API_KEY';
-        // const scopes = 'https://www.googleapis.com/auth/calendar';
-        
         // Authorize owner's Google Calendar
-        handleAuth();    
-  
-        function handleClientLoad() {
-          gapi.client.setApiKey(apiKey);
-          window.setTimeout(checkAuth,1);
-          checkAuth();
-        }
-  
-        function checkAuth() {
-          gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true},
-              handleAuthResult);
-        }
-  
-        function handleAuthResult(authResult) {
-          if (authResult) {
-            createCalendarEvent(data);
-          } else {
-            alert('Event not generated')
-          }
-        }
-  
-        function handleAuth(event) {
-          gapi.auth.authorize(
-              {client_id: clientId, scope: scopes, immediate: true},
-              handleAuthResult);
-          return false;
-        }
-        
-        function createCalendarEvent(data) {
-          gapi.client.load('calendar', 'v3', function() {
-            const buyer = getFormInputInfo()            
-            const tour = purchasedTourInfo()
-            const convertedDate = convertDate(buyer.date)
-            const orderNumber = data.orderID
-            console.log(buyer, tour)
-            
-            writeThankYouNote(buyer, tour, convertedDate, orderNumber)
-  
-// Email markup for letter to send with Google calendar invite to buyer
-const emailMarkup = `
-Dear ${buyer.firstName} ${buyer.lastName},
-
-Thank you for choosing to book your adventures with Ocean Tigers Dive House.
-
-<b style="font-size: 44px;">Tour Information:</b> 
-<b>Tour:</b> ${tour.title}
-<b>Summary:</b> ${tour.description}
-<b>Tour Date:</b> ${convertedDate}
-<b>Tour Price: $</b> ${tour.price} USD
-<b>Tour Deposit: $</b> ${tour.deposit} USD
-
-<b>Order No.:</b> ${orderNumber}
-
-<b>Your Information:</b>
-<b>Name:</b> ${buyer.firstName} ${buyer.lastName}
-<b>Email:</b> ${buyer.email}
-<b>Phone:</b> +${buyer.phone}
-
-Please review the information above and if anything is incorrect, or if you have any additional questions, please email us at oceantigersdivehouse@gmail.com.  
-
-We look forward to joining you in this incredible adventure.
-
-Sincerely,
-
-The Ocean Tigers Dive House Staff
-
-
-`;
-            
-            const event = {
-              // 'id': eventID, // Will be automaticall generated
-              'summary': `${tour.title}, ${orderNumber}`,
-              'location': tour.location,
-              'description': emailMarkup,
-              'start': {
-                'date': buyer.date,
-                'timeZone': 'America/Los_Angeles'
-              },
-              'end': {
-                'date': buyer.date,
-                'timeZone': 'America/Los_Angeles'
-              },
-              'attendees': [
-                {'email': buyer.email}
-              ]
-            };
-            
-            const request = gapi.client.calendar.events.insert({
-              'calendarId': 'primary',
-              'sendNotifications': true,
-              'sendUpdates': 'all',
-              'resource': event
-            })
-            
-            request.execute(function(event) {
-              console.log(event)
-            })
-          })
-
-          function writeThankYouNote(buyer, tour, date, orderNumber) {
-            console.log(buyer, tour)
-            console.log("writing thank you note")
-            const thankYou = document.querySelector('#thank-you')
-            const thankYouModalMarkup = `
-              <div class="p-3">
-                <p>Dear ${buyer.firstName} ${buyer.lastName},</p>
-                <p>Thank you for choosing to book your adventures with Ocean Tigers Dive House.</p>
-                <p>An email has been sent to you with a Google Calendar invite to this event.<p>
-
-                <p class="m-0"><b >Tour Information:</b></p>
-                <p class="m-0"><b>Tour:</b> ${tour.title}</p>
-                <p class="m-0"><b>Tour Date:</b> ${date}</p>
-                <p class="m-0"><b>Tour Price: $</b> ${tour.price} USD</p>
-                <p class="m-0"><b>Deopsit: $</b> ${tour.deposit} USD</p>
-                <p class="m-0"><b>Order No.:</b> ${orderNumber}</p>
-                <br>
-                <p class="m-0"><b>Your Information:</b></p>
-                <p class="m-0"><b>Name:</b> ${buyer.firstName} ${buyer.lastName}</p>        
-                <p class="m-0"><b>Email:</b> ${buyer.email}</p>
-                <p class="m-0"><b>Phone:</b> +${buyer.phone}</p>
-                <br>
-                <p>Please review the information above and if anything is incorrect, or if you have any additional questions, please email us at 
-                  <a href="mailto:oceantigersdivehouse@gmail.com?Subject=${tour.title}%20${orderNumber}" target="_top">oceantigers@gmail.com</a>.
-                </P>  
-                <p>We look forward to joining you in this incredible adventure on ${date}.</p>
-                
-                <div class="row align-items-center">
-                  <div class="col-8">
-                    <p>Sincerely,</p>
-                    <p>The Ocean Tigers Dive House Staff</p>
-                  </div>
-                  <div class="col-4">
-                    <img src="https://www.datocms-assets.com/9161/1549031775-logo-transparent-fish.png" class="img img-responsive img-fluid" alt="OTDH Logo" />
-                  </div>
-                </div>
-              </div>
-            `;
-            thankYou.innerHTML = thankYouModalMarkup
-          }
-        }
+        // handleAuth() 
         // Hide modals
-        $('#checkoutModal').modal('hide')
-        $('#contactModal').modal('hide') 
-        // Show thank you modal
-        $('#thankYouModal').modal('show')
+        hideCheckoutModal()
+        hideContactModal()
+      //   // Show thank you modal
+        showThankYouModal()
       })
     }
   }, '#paypal-button-container')
-  
-// Query Selectors and getElements
-const cardsDiv = document.getElementById('tour-cards')
-const checkoutForm = document.querySelectorAll('.checkout')
-const contactSubmit = document.getElementById('submit-contact-info')
+// const clientId = 'CLIENT_ID';
+// const apiKey = 'API_KEY';
+// const scopes = 'https://www.googleapis.com/auth/calendar';
 
+// Authorize owner's Google Calendar
+
+// function handleClientLoad() {
+//   gapi.client.setApiKey(apiKey);
+//   window.setTimeout(checkAuth,1);
+//   checkAuth();
+// }
+  
+// function checkAuth() {
+//   gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true},
+//       handleAuthResult);
+// }
+
+// function handleAuthResult(authResult) {
+//   if (authResult) {
+//     createCalendarEvent();
+//   } else {
+//     alert('Event not generated')
+//   }
+// }
+
+// function handleAuth() {
+//   gapi.auth.authorize(
+//       {client_id: clientId, scope: scopes, immediate: false},
+//       handleAuthResult);
+//   return false;
+// }
+
+// function createCalendarEvent() {
+//   gapi.client.load('calendar', 'v3', function() {
+//     const buyer = getFormInputInfo()            
+//     const tour = purchasedTourInfo()
+//     const convertedDate = convertDate(buyer.date)
+//     const orderNumber = document.getElementById('orderNumber').innerText
+    
+//     writeThankYouNote(buyer, tour, convertedDate, orderNumber)
+  
+// // Email markup for letter to send with Google calendar invite to buyer
+// const emailMarkup = `
+// Dear ${buyer.firstName} ${buyer.lastName},
+
+// Thank you for choosing to book your adventures with Ocean Tigers Dive House.
+
+// <b style="font-size: 44px;">Tour Information:</b> 
+// <b>Tour:</b> ${tour.title}
+// <b>Summary:</b> ${tour.description}
+// <b>Tour Date:</b> ${convertedDate}
+// <b>Tour Price: $</b> ${tour.price} USD
+// <b>Tour Deposit: $</b> ${tour.deposit} USD
+
+// <b>Order No.:</b> ${orderNumber}
+
+// <b>Your Information:</b>
+// <b>Name:</b> ${buyer.firstName} ${buyer.lastName}
+// <b>Email:</b> ${buyer.email}
+// <b>Phone:</b> +${buyer.phone}
+
+// Please review the information above and if anything is incorrect, or if you have any additional questions, please email us at oceantigersdivehouse@gmail.com.  
+
+// We look forward to joining you in this incredible adventure.
+
+// Sincerely,
+
+// The Ocean Tigers Dive House Staff
+
+
+// `;
+            
+//     const event = {
+//       // 'id': eventID, // Will be automaticall generated
+//       'summary': `${tour.title}, ${orderNumber}`,
+//       'location': tour.location,
+//       'description': emailMarkup,
+//       'start': {
+//         'date': buyer.date,
+//         'timeZone': 'America/Los_Angeles'
+//       },
+//       'end': {
+//         'date': buyer.date,
+//         'timeZone': 'America/Los_Angeles'
+//       },
+//       'attendees': [
+//         {'email': buyer.email}
+//       ]
+//     };
+    
+//     const request = gapi.client.calendar.events.insert({
+//       'calendarId': 'primary',
+//       'sendNotifications': true,
+//       'sendUpdates': 'all',
+//       'resource': event
+//     })
+    
+//     request.execute(function(event) {
+//       console.log(event)
+//     })
+//   })
+//   // Hide modals
+//   hideCheckoutModal()
+//   hideContactModal()
+//   // Show thank you modal
+//   showThankYouModal()
+// }
+
+// function writeThankYouNote(buyer, tour, date, orderNumber) {
+//   console.log(buyer, tour)
+//   console.log("writing thank you note")
+//   const thankYou = document.querySelector('#thank-you')
+//   const thankYouModalMarkup = `
+//     <div class="p-3">
+//       <p>Dear ${buyer.firstName} ${buyer.lastName},</p>
+//       <p>Thank you for choosing to book your adventures with Ocean Tigers Dive House.</p>
+//       <p>An email has been sent to you with a Google Calendar invite to this event.<p>
+
+//       <p class="m-0"><b >Tour Information:</b></p>
+//       <p class="m-0"><b>Tour:</b> ${tour.title}</p>
+//       <p class="m-0"><b>Tour Date:</b> ${date}</p>
+//       <p class="m-0"><b>Tour Price: $</b> ${tour.price} USD</p>
+//       <p class="m-0"><b>Deopsit: $</b> ${tour.deposit} USD</p>
+//       <p class="m-0"><b>Order No.:</b> ${orderNumber}</p>
+//       <br>
+//       <p class="m-0"><b>Your Information:</b></p>
+//       <p class="m-0"><b>Name:</b> ${buyer.firstName} ${buyer.lastName}</p>        
+//       <p class="m-0"><b>Email:</b> ${buyer.email}</p>
+//       <p class="m-0"><b>Phone:</b> +${buyer.phone}</p>
+//       <br>
+//       <p>Please review the information above and if anything is incorrect, or if you have any additional questions, please email us at 
+//         <a href="mailto:oceantigersdivehouse@gmail.com?Subject=${tour.title}%20${orderNumber}" target="_top">oceantigers@gmail.com</a>.
+//       </P>  
+//       <p>We look forward to joining you in this incredible adventure on ${date}.</p>
+      
+//       <div class="row align-items-center">
+//         <div class="col-8">
+//           <p>Sincerely,</p>
+//           <p>The Ocean Tigers Dive House Staff</p>
+//         </div>
+//         <div class="col-4">
+//           <img src="https://www.datocms-assets.com/9161/1549031775-logo-transparent-fish.png" class="img img-responsive img-fluid" alt="OTDH Logo" />
+//         </div>
+//       </div>
+//     </div>
+//   `;
+//   thankYou.innerHTML = thankYouModalMarkup
+// }
+  
 // jQuery serializeArray() to target form and put the name: value fields to return data object
 function getFormInputInfo() {
   const myForm = $("form").serializeArray()
@@ -240,16 +241,38 @@ function purchasedTourInfo() {
   } 
 }
 
-// Listen for Buy Now click event
-cardsDiv.addEventListener('click', openContactForm) 
-
 function openContactForm(e) {
-  emailHelp.style.display = 'none'
-
   // Get chosen tour and pass it along to addTourToCheckout()
   const tour = getChosenTourInfo(e)
   addTourToCheckout(tour)
+  preventPastDate()
+  // Hide email invalid message until needed.
+  const emailHelp = document.getElementById('emailHelp')
+  emailHelp.style.display = 'none'
+  // Target continue to checkout button and prevent the default action
+  const buyerForm = document.getElementById('buyer-form')
+  buyerForm.addEventListener('submit', function(e){ e.preventDefault() })
+  // Listen for validation on keyup event
+  const formFields = document.querySelectorAll('.formField')
+  formFields.forEach(field => $(field).on('keyup change', validateFields))
+  // Listen for validation on keyup event
+  const email = document.getElementById('email')
+  email.addEventListener('keyup', validateEmail)
+  // Validate email while entering it and when the submit button is pressed
+  // Event Listeners for form fields validation
+  const submitContactBtn = document.getElementById('submit-contact-info')
+  submitContactBtn.addEventListener('click', validateEmail)
 }
+
+// Modal Actions
+function toggleContactModal() { $('#contactModal').modal('toggle') }
+function hideContactModal() { $('#contactModal').modal('hide') }
+function showContactModal() { $('#contactModal').modal('show') }
+function toggleCheckoutModal() { $('#checkoutModal').modal('toggle') }
+function hideCheckoutModal() { $('#checkoutModal').modal('hide') }
+function showCheckoutModal() { $('#checkoutModal').modal('show') }
+function showThankYouModal() { $('#thankYouModal').modal('show') }
+
 // with chosen tour, display it on the contact form and checkout form
 function addTourToCheckout(tour) {
   const shoppingCart = document.querySelectorAll('.shopping-cart')
@@ -296,9 +319,7 @@ function addCustomerToCheckout() {
   const customer = document.querySelector('.customer-info')
   const buyer = getFormInputInfo()
   const convertedDate = convertDate(buyer.date)
-  let state = buyer.state
-  if (state == "N/A") { state = "" }
-
+  
   const markupTourCard = `
     <h4 class="m-0 mt-3 mb-3 text-underline">Contact Information:</h4>
     <p class="m-0"><span class="customer-first-name">${buyer.firstName}</span>&nbsp;<span class="customer-last-name">${buyer.lastName}</span></p>
@@ -309,17 +330,8 @@ function addCustomerToCheckout() {
   customer.innerHTML = markupTourCard
 }
 
-// Validate email while entering it and when the submit button is pressed
-// Event Listeners for form fields validation
-const email = document.getElementById('email')
-const submitContactBtn = document.getElementById('submit-contact-info')
-const formFields = document.querySelectorAll('.formField')
-
-formFields.forEach(field => $(field).on('keyup change', validateFields))
-email.addEventListener('keyup', validateEmail)
-submitContactBtn.addEventListener('click', validateEmail)
-
 function validateEmail() {
+  const email = document.getElementById('email')
   const emailHelp = document.getElementById('emailHelp')
   const emailRegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
   
@@ -335,6 +347,7 @@ function validateEmail() {
 }
 
 function validateFields() {
+  const formFields = document.querySelectorAll('.formField')
   formFields.forEach(field => {
     field.classList.add('invalid')
     if (field.value !== '' ) {
@@ -352,28 +365,26 @@ function validateForm() {
 // Hide contact modal and show the checkout modal with paypal buttons
 function checkOut(){
   addCustomerToCheckout()
-  $('#checkoutModal').modal('toggle');
-  $('#contactModal').modal('toggle');
+  $('#checkoutModal').modal('toggle')
+  toggleContactModal()
+  // Go back button closes the checkout modal and returns to the contact form
+  const goBackBtn = document.getElementById('goBackBtn')
+  goBackBtn.addEventListener('click', toggleContactModal)
 }
 
-// Go back button closes the checkout modal and returns to the contact form
-$('#goBackBtn').on('click', function() {
-  $('#contactModal').modal('toggle');
-})
+function preventPastDate() {
+  // Prevent customer from choosing past dates
+  const dt = new Date()
+  const yyyy = dt.getFullYear().toString()
+  const mm = (dt.getMonth() + 1).toString() // getMonth() is zero-based
+  const dd  = dt.getDate().toString()
+  const min = yyyy + '-' + (mm[1] ? mm : "0" + mm[0]) + '-' + (dd[1] ? dd : "0" + dd[0]) // padding
+  $('#tour-date').prop('min', min)
+}
 
-// Target continue to checkout button and prevent the default action
-$("#buyer-form").submit(function(e){ e.preventDefault() });
-
-// Prevent customer from choosing past dates
-const dt = new Date()
-const yyyy = dt.getFullYear().toString()
-const mm = (dt.getMonth() + 1).toString() // getMonth() is zero-based
-const dd  = dt.getDate().toString()
-const min = yyyy + '-' + (mm[1] ? mm : "0" + mm[0]) + '-' + (dd[1] ? dd : "0" + dd[0]) // padding
-$('#tour-date').prop('min', min)
-  
 // Add tour cards to the DOM based on the data from datoCMS
 function populateCards(packages) {
+  const cardsDiv = document.getElementById('tour-cards')
   packages.forEach(p => {
     const markup = `
       <div class="col-lg-4 col-md-6 mt-3">
@@ -398,6 +409,7 @@ function populateCards(packages) {
 
 // HTTP fecth call to database and retrieving all packages using graphql
 function getPackages() {
+  const token = '60ce28d9190500bbe827ebb7766ffa';
   // const token = 'DATO_CMS_TOKEN';
   // Fetch packages from DatoCMS
   fetch(
@@ -458,6 +470,11 @@ function convertDate(date) {
 
 function init() { 
   getPackages() 
+
+  // Listen for Buy Now click event
+  const cardsDiv = document.getElementById('tour-cards')
+  cardsDiv.addEventListener('click', openContactForm) 
+
   // $('#checkoutModal').modal('show')
   // $('#contactModal').modal('show') 
   // $('#thankYouModal').modal('show')
@@ -465,5 +482,4 @@ function init() {
 init()
 
 
-  
   
