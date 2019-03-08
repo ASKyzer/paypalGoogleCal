c1429959a68e571623e18396b94a293741eda720
Adrian Kyzer <adriankyzer@gmail.com>
2019-03-08 09:15:44 +0100

    reset form

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ paypal.Button.render({
         const $form = $("#buyer-form");
         $.post($form.attr("action"), $form.serialize()).then(function() {
           alert("Thank you!");
-          $form.reset()
+          document.getElementById('buyer-form').reset()
         });
       })
     }
